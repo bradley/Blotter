42c22a0867401a48e2b0975d24b310937033e009
bradley <bradley.j.griffith@gmail.com>
2016-02-27 19:49:02 -0800

    rendering 2d wip

--- blotter.js
+++ blotter.js
@@ -655,6 +655,18 @@ if ( typeof module === 'object' ) {
     this.init(material);
   };
   Blotter.Renderer.prototype = function() {
+    function _createOutCanvasCtx(element) {
+      var testText = this.material.mapper.texts[35], size = this.material.mapper.sizeForText(testText), canvas = blotter_CanvasUtils.hiDpiCanvas(size.w, size.h);
+      element.appendChild(canvas);
+      return canvas.getContext("2d");
+    }
+    function _renderToOutCanvasCtx() {
+      var testText = this.material.mapper.texts[35], size = this.material.mapper.sizeForText(testText);
+      if (this.outCanvasCtx) {
+        this.outCanvasCtx.clearRect(0, 0, size.w, size.h);
+        this.outCanvasCtx.drawImage(this.domElement, size.fit.x * this.pixelRatio, size.fit.y * this.pixelRatio, size.w * this.pixelRatio, size.h * this.pixelRatio, 0, 0, size.w, size.h);
+      }
+    }
     return {
       constructor: Blotter.Renderer,
       init: function(material) {
@@ -663,7 +675,7 @@ if ( typeof module === 'object' ) {
           blotter_Messaging.throwError("Blotter.Renderer", "device does not support webgl");
         }
         if (!material.threeMaterial) {
-          blotter_Messaging.throwError("Blotter.Renderer", "material does not expose property threeMaterial. Did you forget to call #load on material before instantiating Blotter.Renderer?");
+          blotter_Messaging.throwError("Blotter.Renderer", "material does not expose property threeMaterial. Did you forget to call #load on your Blotter.Material object before instantiating Blotter.Renderer?");
         }
         this.pixelRatio = blotter_CanvasUtils.pixelRatio();
         this.ratioAdjustedWidth = width * this.pixelRatio;
@@ -697,6 +709,7 @@ if ( typeof module === 'object' ) {
       loop: function() {
         var self = this;
         this.renderer.render(this.scene, this.camera);
+        _renderToOutCanvasCtx.call(this);
         this.currentAnimationLoop = blotter_Animation.requestAnimationFrame(function() {
           self.loop();
         });
@@ -711,6 +724,9 @@ if ( typeof module === 'object' ) {
         this.stop();
         this.renderer = null;
         this.canvas.remove();
+      },
+      appendTo: function(element) {
+        this.outCanvasCtx = _createOutCanvasCtx.call(this, element);
       }
     };
   }();
--- src/renderer/Renderer.js
+++ src/renderer/Renderer.js
@@ -10,6 +10,23 @@ Blotter.Renderer = function (material) {
 
 Blotter.Renderer.prototype = (function () {
 
+  function _createOutCanvasCtx (element) {
+    var testText = this.material.mapper.texts[35],
+        size = this.material.mapper.sizeForText(testText),
+        canvas = blotter_CanvasUtils.hiDpiCanvas(size.w, size.h);
+    element.appendChild(canvas);
+    return canvas.getContext("2d");
+  }
+
+  function _renderToOutCanvasCtx () {
+    var testText = this.material.mapper.texts[35],
+        size = this.material.mapper.sizeForText(testText);
+    if (this.outCanvasCtx) {
+      this.outCanvasCtx.clearRect(0, 0, size.w, size.h);
+      this.outCanvasCtx.drawImage(this.domElement, size.fit.x * this.pixelRatio, size.fit.y * this.pixelRatio, size.w * this.pixelRatio, size.h * this.pixelRatio, 0, 0, size.w, size.h);
+    }
+  }
+
   return {
 
     constructor : Blotter.Renderer,
@@ -24,7 +41,7 @@ Blotter.Renderer.prototype = (function () {
 
       if (!material.threeMaterial) {
         blotter_Messaging.throwError("Blotter.Renderer",
-          "material does not expose property threeMaterial. Did you forget to call #load on material before instantiating Blotter.Renderer?");
+          "material does not expose property threeMaterial. Did you forget to call #load on your Blotter.Material object before instantiating Blotter.Renderer?");
       }
 
       this.pixelRatio = blotter_CanvasUtils.pixelRatio();
@@ -66,6 +83,8 @@ Blotter.Renderer.prototype = (function () {
       var self = this;
       this.renderer.render(this.scene, this.camera);
 
+      _renderToOutCanvasCtx.call(this);
+
       this.currentAnimationLoop = blotter_Animation.requestAnimationFrame(function () {
         self.loop();
       });
@@ -82,6 +101,10 @@ Blotter.Renderer.prototype = (function () {
       this.stop();
       this.renderer = null;
       this.canvas.remove();
-    }
+    },
+
+    appendTo : function (element) {
+      this.outCanvasCtx = _createOutCanvasCtx.call(this, element);
+    },
   }
 })();
