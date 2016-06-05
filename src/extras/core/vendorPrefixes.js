7eb65ed1a68e8b4a8e0a0f74fba9291bf9b2ac09
bradley <bradley.j.griffith@gmail.com>
2016-06-05 11:54:55 -0700

    fresh pages branch

--- src/extras/core/vendorPrefixes.js
+++
@@ -1,7 +0,0 @@
-(function(Blotter, _, THREE, Detector, requestAnimationFrame, EventEmitter, GrowingPacker, setImmediate) {
-
-  Blotter.VendorPrefixes = ["ms", "moz", "webkit", "o"];
-
-})(
-  this.Blotter, this._, this.THREE, this.Detector, this.requestAnimationFrame, this.EventEmitter, this.GrowingPacker, this.setImmediate
-);
