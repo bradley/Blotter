6dc3b87b131c9a6ed19f40048b84f1c5097cabc6
Bradley Griffith <bradley@Bradleys-MacBook-Pro.local>
2017-05-29 17:06:08 -0500

    adding liquid

--- src/assets/shaders/random.js
+++ src/assets/shaders/random.js
@@ -10,11 +10,11 @@
     "//",
     "",
     "float random (in float _x) {",
-	"    return fract(sin(_x)*1e4);",
-	"}",
-	"",
-	"float random (in vec2 co) {",
-	"    return fract(sin(dot(co.xy,vec2(12.9898,78.233)))*43758.5453);",
+  	"    return fract(sin(_x)*1e4);",
+  	"}",
+  	"",
+  	"float random (in vec2 co) {",
+  	"    return fract(sin(dot(co.xy,vec2(12.9898,78.233)))*43758.5453);",
 	"}"
   ].join("\n");
 
