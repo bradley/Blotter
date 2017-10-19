5298117ed82e833d1acded6344ec8fb7c30ae178
bradley <bradley.j.griffith@gmail.com>
2017-10-18 19:06:43 -0700

    finalizing dots material

--- build/filters/dotsMaterial.js
+++ build/filters/dotsMaterial.js
@@ -1,12 +1,12 @@
 (function(Blotter, _) {
 
-  Blotter.SparkleGlitchMaterial = function() {
+  Blotter.DotsMaterial = function() {
     Blotter.Material.apply(this, arguments);
   };
 
-  Blotter.SparkleGlitchMaterial.prototype = Object.create(Blotter.Material.prototype);
+  Blotter.DotsMaterial.prototype = Object.create(Blotter.Material.prototype);
 
-  Blotter._extendWithGettersSetters(Blotter.SparkleGlitchMaterial.prototype, (function () {
+  Blotter._extendWithGettersSetters(Blotter.DotsMaterial.prototype, (function () {
 
     function _mainImageSrc () {
       var mainImageSrc = [
@@ -16,8 +16,7 @@
         "    return fract(sin(vec2(dot(p, vec2(127.1, 311.7)), dot(p, vec2(269.5, 183.3)))) * 43758.5453);",
         "}",
 
-
-        "float isParticle(vec2 fragCoord, float pointRadius, float pointCellWidth) {    ",
+        "float isParticle(out vec3 particleColor, vec2 fragCoord, float pointRadius, float pointCellWidth, float dodge, vec2 dodgePosition, float dodgeSpread, float speed) {    ",
         "    if (pointCellWidth == 0.0) { return 0.0; };",
 
         "    vec2 uv = fragCoord.xy / uResolution.xy;",
@@ -33,6 +32,10 @@
 
         "    float minDist = 1.0;  // minimun distance",
 
+        "    vec4 baseSample = textTexture(cellUv);",
+        "    float maxWeight = 0.0;",
+        "    particleColor = baseSample.rgb;",
+
         "    for (int y= -1; y <= 1; y++) {",
         "        for (int x= -1; x <= 1; x++) {",
         "            // Neighbor place in the grid",
@@ -41,16 +44,30 @@
         "            // Random position from current + neighbor place in the grid",
         "            vec2 point = random2(iUv + neighbor);",
 
-        "            // Get cell weighting from edgeDistanceBlur",
+        "            // Get cell weighting from cell's center alpha",
         "            vec2 cellRowCol = floor(fragCoord / pointCellWidth) + neighbor;",
         "            vec2 cellFragCoord = ((cellRowCol * pointCellWidth) + (pointCellWidth / 2.0));",
-        "            float cellWeight = textTexture(cellFragCoord / uResolution.xy).a;",
+        "            vec4 cellSample = textTexture(cellFragCoord / uResolution.xy);",
+        "            float cellWeight = cellSample.a;",
 
-        "            // Apply weighting to noise",
-        "            cellWeight = step(cellWeight, random(cellRowCol));",
+        "            if (cellWeight < 1.0) {",
+        "               // If the cell is not fully within our text, we should disregard it",
+        "               continue;",
+        "            }",
+
+        "            maxWeight = max(maxWeight, cellWeight);",
+        "            if (cellWeight == maxWeight) {",
+        "                particleColor = cellSample.rgb;",
+        "            }",
+
+        "            float distanceFromDodge = distance(dodgePosition * uResolution.xy, cellFragCoord) / uResolution.y;",
+        "            distanceFromDodge = 1.0 - smoothstep(0.0, dodgeSpread, distanceFromDodge);",
+
+        "            // Apply weighting to noise and dodge if dodge is set to 1.0",
+        "            cellWeight = step(cellWeight, random(cellRowCol)) + (distanceFromDodge * dodge);",
 
         "            // Animate the point",
-        "            point = 0.5 + 0.75 * sin(uGlobalTime + 6.2831 * point);",
+        "            point = 0.5 + 0.75 * sin((uGlobalTime * speed) + 6.2831 * point);",
 
         "            // Vector between the pixel and the point",
         "            vec2 diff = neighbor + point - fUv;",
@@ -61,15 +78,16 @@
 
         "            // Keep the closer distance",
         "            minDist = min(minDist, dist);",
+        "            if(minDist == dist) {",
+        "               //particleColor = cellSample.rgb;",
+        "            }",
         "        }",
         "    }",
 
-        "    float test = pointRadius / pointCellWidth;",
 
         "    float pointEasing = pointRadiusOfCell - (1.0 / pointCellWidth);",
 
         "    float isParticle = 1.0 - smoothstep(pointEasing, pointRadiusOfCell, minDist);",
-
 
         "    return isParticle;",
         "}",
@@ -79,10 +97,12 @@
 
         "    float pointCellWidth = max(uPointCellWidth, 1.0);",
         "    float pointRadius = min(uPointRadius, pointCellWidth * 0.7);",
+        "    float dodge = ceil(uDodge);",
 
-        "    float sparkle = isParticle(fragCoord, pointRadius, pointCellWidth);",
+        "    vec3 outColor = vec3(0.0);",
+        "    float sparkle = isParticle(outColor, fragCoord, pointRadius, pointCellWidth, dodge, uDodgePosition, uDodgeSpread, uSpeed);",
 
-        "    mainImage = vec4(vec3(1.0), sparkle);",
+        "    mainImage = vec4(outColor, sparkle);",
         "}"
       ].join("\n");
 
@@ -91,14 +111,17 @@
 
     return {
 
-      constructor : Blotter.SparkleGlitchMaterial,
+      constructor : Blotter.DotsMaterial,
 
       init : function () {
         this.mainImage = _mainImageSrc();
         this.uniforms = {
-          uAnimate : { type : "1f", value : 1.0 },
           uPointRadius : { type : "1f", value : 0.16 },
-          uPointCellWidth : { type : "1f", value : 20.0 }
+          uPointCellWidth : { type : "1f", value : 20.0 },
+          uDodge : { type : "1f", value : 0.0 },
+          uDodgePosition : { type : "2f", value : [0.5, 0.5] },
+          uDodgeSpread : { type : "1f", value : 0.10 },
+          uSpeed : { type : "1f", value : 1.0 }
         };
       }
     };
