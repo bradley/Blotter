ed56b562d161876adb061aaef2360dac84d63b03
bradley <bradley.j.griffith@gmail.com>
2018-01-09 20:27:25 -0800

    Adding logo

--- third_party/requestAnimationFrame.js
+++
@@ -1,9 +0,0 @@
-// shim layer with setTimeout fallback
-window.requestAnimFrame = (function(){
-  return  window.requestAnimationFrame       ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame    ||
-    function( callback ){
-    window.setTimeout(callback, 1000 / 60);
-  };
-})();
